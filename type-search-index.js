eed8c0b2179b543a7c2d59768ccdabc20780776d
Dino <dino15031992@gmail.com>
2023-02-10 00:03:34 +0100

    commit

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"co.develhope.FirstAPI03","l":"FirstApi03Application"},{"p":"co.develhope.FirstAPI03","l":"StringController"}];updateSearchResults();
